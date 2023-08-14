8048c7ef90c52e3094de4ec901b081b6ed04d384
Davit Vardanyan <78792753+davvard@users.noreply.github.com>
2023-08-14 17:23:51 +0400

    Add: USearch

--- src/Storages/MergeTree/MergeTreeIndexHnsw.h
+++ src/Storages/MergeTree/MergeTreeIndexHnsw.h
@@ -2,109 +2,102 @@
 
 #ifdef ENABLE_USEARCH
 
-#include <Storages/MergeTree/ApproximateNearestNeighborIndexesCommon.h>
+#    include <Storages/MergeTree/ApproximateNearestNeighborIndexesCommon.h>
 
-#include <index.hpp>
+#    include <usearch/index_dense.hpp>
+
+namespace DB
+{
+
+template <unum::usearch::metric_kind_t metric_ak>
+class USearchIndexWithSerialization : public unum::usearch::index_dense_t
+{
+    using Base = unum::usearch::index_dense_t;
+
+public:
+    explicit USearchIndexWithSerialization(size_t dimensions);
+    void serialize(WriteBuffer & ostr) const;
+    void deserialize(ReadBuffer & istr);
+    size_t getDimensions() const;
+};
+
+template <unum::usearch::metric_kind_t metric_ak>
+using USearchIndexWithSerializationPtr = std::shared_ptr<USearchIndexWithSerialization<metric_ak>>;
+
+template <unum::usearch::metric_kind_t metric_ak>
+struct MergeTreeIndexGranuleUSearch final : public IMergeTreeIndexGranule
+{
+    MergeTreeIndexGranuleUSearch(const String & index_name_, const Block & index_sample_block_);
+    MergeTreeIndexGranuleUSearch(
+        const String & index_name_, const Block & index_sample_block_, USearchIndexWithSerializationPtr<metric_ak> index_);
+
+    ~MergeTreeIndexGranuleUSearch() override = default;
+
+    void serializeBinary(WriteBuffer & ostr) const override;
+    void deserializeBinary(ReadBuffer & istr, MergeTreeIndexVersion version) override;
+
+    bool empty() const override { return !index.get(); }
+
+    const String index_name;
+    const Block index_sample_block;
+    USearchIndexWithSerializationPtr<metric_ak> index;
+};
+
+template <unum::usearch::metric_kind_t metric_ak>
+struct MergeTreeIndexAggregatorUSearch final : IMergeTreeIndexAggregator
+{
+    MergeTreeIndexAggregatorUSearch(const String & index_name_, const Block & index_sample_block);
+    ~MergeTreeIndexAggregatorUSearch() override = default;
+
+    bool empty() const override { return !index || index->size() == 0; }
+    MergeTreeIndexGranulePtr getGranuleAndReset() override;
+    void update(const Block & block, size_t * pos, size_t limit) override;
+
+    const String index_name;
+    const Block index_sample_block;
+    USearchIndexWithSerializationPtr<metric_ak> index;
+};
 
 
-/// namespace DB
-/// {
-///
-/// template <typename Distance>
-/// class AnnoyIndexWithSerialization : public Annoy::AnnoyIndex<UInt64, Float32, Distance, Annoy::Kiss64Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy>
-/// {
-///     using Base = Annoy::AnnoyIndex<UInt64, Float32, Distance, Annoy::Kiss64Random, Annoy::AnnoyIndexMultiThreadedBuildPolicy>;
-///
-/// public:
-///     explicit AnnoyIndexWithSerialization(size_t dimensions);
-///     void serialize(WriteBuffer & ostr) const;
-///     void deserialize(ReadBuffer & istr);
-///     size_t getDimensions() const;
-/// };
-///
-/// template <typename Distance>
-/// using AnnoyIndexWithSerializationPtr = std::shared_ptr<AnnoyIndexWithSerialization<Distance>>;
-///
-/// template <typename Distance>
-/// struct MergeTreeIndexGranuleAnnoy final : public IMergeTreeIndexGranule
-/// {
-///     MergeTreeIndexGranuleAnnoy(const String & index_name_, const Block & index_sample_block_);
-///     MergeTreeIndexGranuleAnnoy(const String & index_name_, const Block & index_sample_block_, AnnoyIndexWithSerializationPtr<Distance> index_);
-///
-///     ~MergeTreeIndexGranuleAnnoy() override = default;
-///
-///     void serializeBinary(WriteBuffer & ostr) const override;
-///     void deserializeBinary(ReadBuffer & istr, MergeTreeIndexVersion version) override;
-///
-///     bool empty() const override { return !index.get(); }
-///
-///     const String index_name;
-///     const Block index_sample_block;
-///     AnnoyIndexWithSerializationPtr<Distance> index;
-/// };
-///
-/// template <typename Distance>
-/// struct MergeTreeIndexAggregatorAnnoy final : IMergeTreeIndexAggregator
-/// {
-///     MergeTreeIndexAggregatorAnnoy(const String & index_name_, const Block & index_sample_block, UInt64 trees);
-///     ~MergeTreeIndexAggregatorAnnoy() override = default;
-///
-///     bool empty() const override { return !index || index->get_n_items() == 0; }
-///     MergeTreeIndexGranulePtr getGranuleAndReset() override;
-///     void update(const Block & block, size_t * pos, size_t limit) override;
-///
-///     const String index_name;
-///     const Block index_sample_block;
-///     const UInt64 trees;
-///     AnnoyIndexWithSerializationPtr<Distance> index;
-/// };
-///
-///
-/// class MergeTreeIndexConditionAnnoy final : public IMergeTreeIndexConditionApproximateNearestNeighbor
-/// {
-/// public:
-///     MergeTreeIndexConditionAnnoy(
-///         const IndexDescription & index_description,
-///         const SelectQueryInfo & query,
-///         const String & distance_function,
-///         ContextPtr context);
-///
-///     ~MergeTreeIndexConditionAnnoy() override = default;
-///
-///     bool alwaysUnknownOrTrue() const override;
-///     bool mayBeTrueOnGranule(MergeTreeIndexGranulePtr idx_granule) const override;
-///     std::vector<size_t> getUsefulRanges(MergeTreeIndexGranulePtr idx_granule) const override;
-///
-/// private:
-///     template <typename Distance>
-///     std::vector<size_t> getUsefulRangesImpl(MergeTreeIndexGranulePtr idx_granule) const;
-///
-///     const ApproximateNearestNeighborCondition ann_condition;
-///     const String distance_function;
-///     const Int64 search_k;
-/// };
-///
-///
-/// class MergeTreeIndexAnnoy : public IMergeTreeIndex
-/// {
-/// public:
-///
-///     MergeTreeIndexAnnoy(const IndexDescription & index_, UInt64 trees_, const String & distance_function_);
-///
-///     ~MergeTreeIndexAnnoy() override = default;
-///
-///     MergeTreeIndexGranulePtr createIndexGranule() const override;
-///     MergeTreeIndexAggregatorPtr createIndexAggregator() const override;
-///     MergeTreeIndexConditionPtr createIndexCondition(const SelectQueryInfo & query, ContextPtr context) const override;
-///
-///     bool mayBenefitFromIndexForIn(const ASTPtr & /*node*/) const override { return false; }
-///
-/// private:
-///     const UInt64 trees;
-///     const String distance_function;
-/// };
-///
-///
-/// }
+class MergeTreeIndexConditionUSearch final : public IMergeTreeIndexConditionApproximateNearestNeighbor
+{
+public:
+    MergeTreeIndexConditionUSearch(
+        const IndexDescription & index_description, const SelectQueryInfo & query, const String & distance_function, ContextPtr context);
+
+    ~MergeTreeIndexConditionUSearch() override = default;
+
+    bool alwaysUnknownOrTrue() const override;
+    bool mayBeTrueOnGranule(MergeTreeIndexGranulePtr idx_granule) const override;
+    std::vector<size_t> getUsefulRanges(MergeTreeIndexGranulePtr idx_granule) const override;
+
+private:
+    template <unum::usearch::metric_kind_t metric_ak>
+    std::vector<size_t> getUsefulRangesImpl(MergeTreeIndexGranulePtr idx_granule) const;
+
+    const ApproximateNearestNeighborCondition ann_condition;
+    const String distance_function;
+};
+
+
+class MergeTreeIndexUSearch : public IMergeTreeIndex
+{
+public:
+    MergeTreeIndexUSearch(const IndexDescription & index_, const String & distance_function_);
+
+    ~MergeTreeIndexUSearch() override = default;
+
+    MergeTreeIndexGranulePtr createIndexGranule() const override;
+    MergeTreeIndexAggregatorPtr createIndexAggregator() const override;
+    MergeTreeIndexConditionPtr createIndexCondition(const SelectQueryInfo & query, ContextPtr context) const override;
+
+    bool mayBenefitFromIndexForIn(const ASTPtr & /*node*/) const override { return false; }
+
+private:
+    const String distance_function;
+};
+
+
+}
 
 #endif
